056f6bef5af7f910af17fdad0d6cadec35b42486
laluiz <luiz280530@hotmail.com>
2021-05-06 18:57:55 -0300

    primeiro commit

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import {Router, Request, Response, response} from 'express';
+import {UserController} from './controller/UserController';
+import { ActivyController } from './controller/ActivyController';
+import {CourseUnitController} from './controller/CourseUnitController';
+
+const userController = new UserController()
+const activyController = new ActivyController()
+const courseUnitController = new CourseUnitController()
+
+const routes = Router();
+
+routes.post('/user', userController.create);
+routes.post('/activy', activyController.create);
+routes.post('/courseuunit', courseUnitController.create);
+
+export default routes;
