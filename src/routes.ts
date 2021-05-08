c8ff25825097ef0ae1e88cf2f0b22dfdc29d7b5d
laluiz <luiz280530@hotmail.com>
2021-05-07 22:29:55 -0300

    add-authentication

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,21 @@
 import {Router, Request, Response, response} from 'express';
-import {UserController} from './controller/UserController';
+import { UserController} from './controller/UserController';
 import { ActivyController } from './controller/ActivyController';
-import {CourseUnitController} from './controller/CourseUnitController';
-
+import { CourseUnitController} from './controller/CourseUnitController';
+import { AuthenticateController} from './controller/AuthenticateController';
+import  authenticated  from './middlewares/authenticated';
+ 
 const userController = new UserController()
 const activyController = new ActivyController()
 const courseUnitController = new CourseUnitController()
+const authenticateController = new AuthenticateController()
 
 const routes = Router();
 
 routes.post('/user', userController.create);
-routes.post('/activy', activyController.create);
-routes.post('/courseuunit', courseUnitController.create);
+routes.get('/user', authenticated, userController.show);
+routes.post('/auth', authenticateController.create);
+routes.post('/activy', authenticated, activyController.create);
+routes.post('/courseuunit', authenticated, courseUnitController.create);
 
 export default routes;
